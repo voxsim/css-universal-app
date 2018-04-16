1c764ab7e91ae50f76c1eb919cd9d7aa6be66a94
Simon Vocella <voxsim@gmail.com>
2018-04-16 22:00:42 +0200

    add react-native-scripts

---
+++ src/App4.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import styled from "styled-components";
+
+const AppHeader = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+
+const Logo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 80px;
+`;
+
+const AppIntro = styled.p`
+  font-size: large;
+`;
+
+const AppBody = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <AppBody>
+        <AppHeader>
+          <Logo src={logo} alt="logo" />
+          <Title>Welcome to React</Title>
+        </AppHeader>
+        <AppIntro>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </AppIntro>
+      </AppBody>
+    );
+  }
+}
+
+export default App;
